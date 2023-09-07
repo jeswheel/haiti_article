de4e1670b3789b7bca27f9083c1526e7571951ef
Jesse Wheeler <jeswheel@umich.edu>
2023-09-07 12:51:06 -0400

    finished making Makefile to create supplement files

---
+++ si/make_si_file.R
@@ -0,0 +1,3 @@
+args <- commandArgs(trailingOnly = TRUE)
+source('utilityFuns.R')
+make_si_file(args[1])
--- si/utilityFuns.R
+++ si/utilityFuns.R
@@ -6,3 +6,21 @@ remove_start_end <- function(file) {
   writeLines(tmp_file[(start_line+1):(end_line-1)], con = gsub("\\.tex", "Out.tex", file))
 }
 
+make_si_file <- function(file) {
+  template <- readLines('inputs/si_files_template.tex')
+  input_file <- readLines(file)
+
+  if (grepl('Tab', file)) {
+    tab_caption <- which(grepl('\\caption', input_file))
+    input_file <- input_file[-tab_caption]
+  }
+
+  start_line <- which(grepl("%* START", template))
+  end_line <- which(grepl("%* END", template))
+  file_loc <- gsub("inputs/", "", file)
+  new_file <- c(template[1:(start_line - 1)], input_file, template[(end_line + 1):length(template)])
+  writeLines(new_file, con = file_loc)
+
+  has_citation <- any(grepl("\\cite", new_file))
+  if (has_citation) writeLines(as.character(has_citation), con = gsub('.tex', '.tmp', file_loc))
+}
