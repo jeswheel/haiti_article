99b8a129b56508086fe068db4e15ec1af788cb01
Jesse Wheeler <jeswheel@umich.edu>
2024-01-12 11:53:51 -0500

    Fixed cooling rate 0.05 -> 0.5

--- H3profile.R
+++ H3profile.R
@@ -26,12 +26,12 @@ NP <- switch(RUN_LEVEL, 50, 750, 1000)
 NP_EVAL <- switch(RUN_LEVEL, 100, 1000, 2000)
 NREPS_EVAL <- switch(RUN_LEVEL, 3, 6, 10)
 SPAT_REGRESSION <- 0.05
-COOLING <- 0.05
+COOLING <- 0.50
 
 # Create Experiment Registry ----------------------------------------------
 
 reg <- makeExperimentRegistry(
-  file.dir = paste0('model3/profileReg_RL', RUN_LEVEL, '_v2'),
+  file.dir = paste0('model3/profileReg_RL', RUN_LEVEL, '_v3'),
   seed = 739164,
   packages = c("spatPomp", 'haitipkg', 'pomp')
 )
